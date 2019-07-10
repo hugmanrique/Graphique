4c07c77ae2bcdeb0e18afdd743ef622f2c9a942a
Hugo Manrique <hugmanrique@gmail.com>
2019-07-10 21:41:02 +0200

    Add Arrow component

---
+++ src/components/Arrow.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Marker from '../primitives/Marker';
+import Line from '../primitives/Line';
+
+import { useGraphique } from '../context';
+import { pointType } from '../points';
+
+const ARROW_SIZE = 10;
+const ARROW_OFFSET = [ARROW_SIZE - 2, 3];
+
+const generateMarkerId = (from, to) =>
+  `${from[0]}-${from[1]}-${to[0]}-${to[1]}`;
+
+function Arrow({ from, to, markerSize, stroke, ...props }) {
+  const { getCanvasPoint } = useGraphique();
+
+  const fromPoint = getCanvasPoint(from);
+  const toPoint = getCanvasPoint(to);
+
+  // No risk of XSS, the id gets passed as a prop
+  const markerId = generateMarkerId(from, to);
+  const markerViewbox = [markerSize * ARROW_SIZE, markerSize * ARROW_SIZE];
+
+  return (
+    <>
+      <Marker
+        id={markerId}
+        width={ARROW_SIZE}
+        height={ARROW_SIZE}
+        offset={ARROW_OFFSET}
+        viewbox={markerViewbox}
+      >
+        <path d="M0,0 L0,6 L9,3 z" fill={stroke} />
+      </Marker>
+      <Line
+        {...props}
+        from={fromPoint}
+        to={toPoint}
+        stroke={stroke}
+        markerEnd={`url(#${markerId})`}
+      />
+    </>
+  );
+}
+
+Arrow.propTypes = {
+  from: pointType.isRequired,
+  to: pointType.isRequired,
+  markerSize: PropTypes.number,
+  stroke: PropTypes.string
+};
+
+Arrow.defaultProps = {
+  markerSize: 1,
+  stroke: '#718096',
+  strokeWidth: 2
+};
+
+export default React.memo(Arrow);
